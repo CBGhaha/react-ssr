742d1041333c85e6e5447dc705d572e2165e7e6f
CBGhaha <875675253@qq.com>
2018-11-08 19:36:45 +0800

    舍弃没必要的layout、middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import {createStore,applyMiddleware } from 'redux';
-import  ajaxMiddleware from './middleware/ajaxMiddleware.js';
 import { combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import * as reducers from './reducer/index.js';
 
 export default function initStore(initState){
-  return createStore(combineReducers({...reducers}),initState,applyMiddleware(ajaxMiddleware,thunkMiddleware));
+  return createStore(combineReducers({...reducers}),initState,applyMiddleware(thunkMiddleware));
 }
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,9 @@
 import React,{Component}from 'react';
 import {Router,Route,Link,Switch} from 'react-router-dom';
 import AsyncComponent from '../components/asyncComponent';
-import RootLayout from '../layout/rootLayout';
 import Notfind from '../pages/404';
 
-
+//在服务端是不需要异步加载组件的 采用AsyncComponent componentDidMount生命周期反而不会执行拿不到异步的组件
 let Page1 = process.env.NODE_ENV==='ssr'?require('../pages/page1/index.js').default:AsyncComponent(()=>import('../pages/page1/index.js'));
 let Page2 = process.env.NODE_ENV==='ssr'?require('../pages/page2/index.js').default:AsyncComponent(()=>import('../pages/page2/index.js'));
 let Home  = process.env.NODE_ENV==='ssr'?require('../pages/home/index.js').default:AsyncComponent(()=>import('../pages/home/index.js'));
@@ -15,11 +14,6 @@ const links=[
   {routerName:'Page1',url:'/page1'},
   {routerName:'Page2',url:'/page2'}
 ];
-
-
-const LayoutComponent=({component:Component,...reset})=>(
-    <Route {...reset} render={()=>(<RootLayout><Component/></RootLayout>)} />
-)
 
  const myRouter=(history)=>{
     return (
